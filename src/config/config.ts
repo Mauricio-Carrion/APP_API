4636c15f831efb8d16a3f12e6d2d6b24b125d9cd
Mauricio Carrion <mauricio.sjn@gmail.com>
2023-01-07 19:04:26 -0300

    criando classes controller e config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,39 +1,53 @@
 export default class Config {
-  configJson: any
-  database: string
-  databaseName: string
-  firebirdPath: string
-  firebirdDefaultPath: string
-  host: string
-  mySqlPort: number
-  firebirdPort: number
-  configFile: string
-
-  static converteJson: any
+  private configJson: any
+  private database: string
+  private databaseName: string
+  private firebirdPath: string
+  private firebirdDefaultPath: string
+  private host: string
+  private mySqlPort: number
+  private firebirdPort: number
+  private configFile: string
 
   constructor() {
     this.configJson = null
+    this.firebirdDefaultPath = 'C://JF System//Estoque//Dados//JFC.FDB'
     this.configFile = `${__dirname}/config.json`
     this.database = this.configJson.database
     this.databaseName = this.configJson.databaseName
-    this.firebirdPath = this.configJson.firebirdPath
-    this.firebirdDefaultPath = 'C://JF System//Estoque//Dados//JFC.FDB'
+    this.firebirdPath = this.configJson.firebirdPath ? this.configJson.firebirdPath : this.firebirdDefaultPath
     this.host = '127.0.0.1'
     this.mySqlPort = 3306
     this.firebirdPort = 3050
     this.converteConfigJson()
   }
 
-  converteConfigJson(): void {
+  private converteConfigJson(): void {
     let configObj = JSON.parse(this.configFile)
     this.configJson = configObj
   }
 
-  get
+  get db() {
+    return this.database
+  }
+
+  get dbName() {
+    return this.databaseName
+  }
+
+  get fbPath() {
+    return this.firebirdPath
+  }
+
+  get dbhost() {
+    return this.host
+  }
+
+  get mysqlPort() {
+    return this.mySqlPort
+  }
+
+  get fbPort() {
+    return this.firebirdPort
+  }
 }
-    
-
-
-
-
-
--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,20 +1,32 @@
 import { Request, Response } from 'express';
 import Config from '../config/Config'
 
+const config = new Config
 export default class Controller {
   static getProduto: any;
   static putSaldoProduto: any;
 
-  async getProduto(req: Request, res: Response): Promise<void> {
+  private async getProduto(req: Request, res: Response): Promise<void> {
     let codigo = req.params.codigo
-    let produto
 
-    // produto = await Model.getProdutoFirebirdModel(codigo)
+    try {
+      let produtoResposta
 
-    // produto = await Model.getProdutoMysqlModel(codigo)
+      if (config.db === "Firebird") {
+        // await produtoResposta = await Model.getProdutoFirebirdModel(codigo)
+      } else {
+        // await produtoResposta = await Model.getProdutoMysqlModel(codigo)
+      }
+
+      res.status(200).send(produtoResposta)
+    } catch (error) {
+
+      res.status(502).send({ msg: 'Ocorreu um erro no servidor, tente mais tarde!' })
+
+    }
   }
 
-  async putSaldoProduto(codigo: number, saldo: number): Promise<void> {
+  private async putSaldoProduto(codigo: number, saldo: number): Promise<void> {
 
   }
 }
